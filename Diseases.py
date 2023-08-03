5df23dc6ab1e20de151f126d2ee942b08ed76b1e
SpacetimeBAB <108306081+SpacetimeBAB@users.noreply.github.com>
2023-08-03 19:38:53 -0400

    Add automated patient data and version control integration
    Implemented a script for auto-generation of patient data in 'patient_names.txt', creating a more efficient way to populate the file for testing. Also added a new '.xml' file to enable Git as the version control system in the '.idea' directory, supporting better project tracking. Furthermore, a new python file 'Diseases.py' was added to store and manage diseases related data. The enhancements allow for a more organized workspace and smoother testing operations.

---
+++ Diseases.py
@@ -0,0 +1,27 @@
+import random
+
+DiseaseList = [
+    "Influenza", "Common cold", "Tuberculosis", "HIV/AIDS", "Sprained ankle", "Malaria", "COVID-19", "Cholera",
+    "Measles", "Diabetes mellitus", "Hypertension (High blood pressure)", "Cancer", "Alzheimer's disease",
+    "Fractured bone", "Asthma", "Chronic obstructive pulmonary disease (COPD)", "Hepatitis B", "Concussion",
+    "Dengue fever", "Ebola virus disease", "Leukemia", "Schizophrenia", "Bipolar disorder", "Epilepsy",
+    "Rheumatoid arthritis", "Osteoarthritis", "Cystic fibrosis", "Multiple sclerosis", "Sickle cell anemia",
+    "Osteoporosis", "Glaucoma", "Cataracts", "Eczema", "Psoriasis", "Irritable bowel syndrome (IBS)", "Crohn's disease",
+    "Ulcerative colitis", "Celiac disease", "Gout", "Sprained wrist", "Chlamydia", "Gonorrhea", "Syphilis",
+    "Cholecystitis (Gallbladder inflammation)", "Appendicitis", "Pancreatitis",
+    "Gastroesophageal reflux disease (GERD)", "Hemorrhoids", "Pneumonia", "Bronchitis", "Encephalitis", "Epiglottitis",
+    "Myocardial infarction (Heart attack)", "Congestive heart failure", "Atherosclerosis", "Deep vein thrombosis (DVT)",
+    "Pulmonary embolism", "Nephritis (Kidney inflammation)", "Renal failure", "Polycystic kidney disease",
+    "Uterine fibroids", "Endometriosis", "Polycystic ovarian syndrome (PCOS)", "Prostate cancer", "Testicular cancer",
+    "Ovarian cancer", "Breast cancer", "Colon cancer", "Esophageal cancer", "Leukemia", "Hodgkin's lymphoma",
+    "Non-Hodgkin's lymphoma", "Melanoma", "Hypothyroidism", "Hyperthyroidism", "Addison's disease", "Graves' disease",
+    "Systemic lupus erythematosus (SLE)", "Fibromyalgia", "Chronic fatigue syndrome (CFS)", "Sarcoidosis",
+    "Myasthenia gravis", "Amyotrophic lateral sclerosis (ALS)", "Cerebral palsy", "Down syndrome", "Turner syndrome",
+    "Klinefelter syndrome", "Sickle cell disease", "Marfan syndrome", "Sprained knee", "Huntington's disease",
+    "Hemophilia", "Thalassemia", "Gaucher's disease", "Fractured rib", "Hemochromatosis", "Cysticercosis", "Leprosy",
+    "Yellow fever", "Rabies",
+]
+
+
+def getRandomDisease():
+    return random.choice(DiseaseList)
--- nameAdder.py
+++ nameAdder.py
@@ -1,4 +1,24 @@
 # Adds names which then patients.py will check whether the person ur logging in as is a patient
-nameToAdd = input("What is your name to be registered under?")
-with open("patient_names.txt", "a") as file:
-    file.write(nameToAdd + "\n")
+# nameToAdd = input("What is your name to be registered under?")
+# with open("patient_names.txt", "a") as file:
+# file.write(nameToAdd + "\n")
+
+import random
+from faker import Faker
+import Diseases
+
+fake = Faker()
+
+
+def get_random_age(min_age=18, max_age=90):
+    return random.randint(min_age, max_age)
+
+for i in range(100) :
+    with open("patient_names.txt", "a") as file:
+        file.write(fake.name() + ", ")
+        file.write(str(get_random_age()) + ", ")
+        file.write(Diseases.getRandomDisease() + "\n")
+
+
+
+
