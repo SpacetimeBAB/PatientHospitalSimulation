74680d6e9f036814957037ec65fc1e24908b85f1
SpacetimeBAB <108306081+SpacetimeBAB@users.noreply.github.com>
2023-08-07 14:40:56 -0400

    added gender prediction algorithm for the patient's names

--- nameAdder.py
+++ nameAdder.py
@@ -1,47 +1,50 @@
-# Adds names which then patients.py will check whether the person ur logging in as is a patient
-# nameToAdd = input("What is your name to be registered under?")
-# with open("patient_names.txt", "a") as file:
-# file.write(nameToAdd + "\n")
 import os
 import random
 import string
-
 from faker import Faker
 import Diseases
+from nltk.corpus import names
 
 fake = Faker()
+
+
+def guess_gender(name):
+    if name.title() in names.words('male.txt'):
+        return 'Male'
+    elif name.title() in names.words('female.txt'):
+        return 'Female'
+    else:
+        return 'Unknown'
 
 
 def get_random_age(min_age=18, max_age=90):
     return random.randint(min_age, max_age)
 
 
-
 def sanitize_name(name):
     valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
     sanitized_name = ''.join(c for c in name if c in valid_chars)
-    return sanitized_name[:255]  # Truncate to allowable length for most file systems
+    return sanitized_name[:255]
 
 
-for i in range(100):
-    name = fake.name()
+for i in range(1):
+    name = fake.name() # generate the full name
+    firstName = name.split()[0] # get the first name from the split of full name
     age = get_random_age()
     disease = Diseases.getRandomDisease()
     status = Diseases.getRandomHealthStatus()
+    gender = guess_gender(firstName) # use the firstName variable for gender prediction
 
-    folder_name = sanitize_name(name)
+    folder_name = sanitize_name(name) # use full name for folder naming
 
-    # Create a new directory for each patient
     os.makedirs(f'patient_records/{folder_name}', exist_ok=True)
-
-    # Create folder for age within patient folder
     os.makedirs(f"patient_records/{folder_name}/age", exist_ok=True)
 
-    # Write the patient's data to a file in their directory
     with open(f'patient_records/{folder_name}/age/{age}', 'w') as file:
         file.write(f"Age: {age}")
 
     with open(f'patient_records/{folder_name}/{disease}', 'w') as file:
         file.write(f"Disease: {disease}\nStatus: {status}")
 
-
+    with open(f"patient_records/{folder_name}/{gender}", "w") as file:
+        file.write(f"Gender: {gender}\n")
